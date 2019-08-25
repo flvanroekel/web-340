c2d3f1212d05c70f224ea1eedb9870dc9cd40046
Faye Van Roekel <flvanroekel@my365.bellevue.edu>
2019-08-25 15:21:29 -0500

    vanroekel-exercise-3.4.js

---
+++ Week 3/putting-it-all-together/vanroekel-exercise-3.4.js
@@ -0,0 +1,47 @@
+/*
+============================================
+; Title:  vanroekel-assignment-3.4.js
+; Author: Faye Van Roekel
+; Date:   25 August 2019
+; Description: Putting it all together
+;===========================================
+*/
+
+var express = require("express");
+var http = require("http");
+var path = require("path");
+var logger = require("morgan");
+
+
+var app = express();
+
+app.set("views", path.resolve(__dirname, "views"));
+
+app.set("view engine", "ejs");
+
+app.use(logger("short"));
+
+app.get("/", function(request, response) {
+  response.render("index", {
+    message: "homepage"
+  });
+});
+app.get("/about", function(request, response) {
+  response.render("about", {
+    message: "about page"
+  });
+});
+app.get("/contact", function(request, response) {
+  response.render("contact", {
+    message: "contact page"
+  });
+});
+app.get("/products", function(request, response) {
+  response.render("products", {
+    message: "products page"
+  });
+});
+
+http.createServer(app).listen(8080, function() {
+  console.log("Application started on port 8080");
+});
