b8a090ddd290b530f1e89b127b7ec398139db05d
Faye Van Roekel <flvanroekel@my365.bellevue.edu>
2019-10-07 13:37:45 -0500

    changes for heroku

--- ems/app.js
+++ ems/app.js
@@ -66,7 +66,7 @@ app.use(function(req, res, next) {
 
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
-app.set('port', process.env.PORT || 8080);
+
 
 app.use(logger("short"));
 app.use(helmet.xssFilter());
@@ -197,6 +197,5 @@ app.listen(app.get("port"), function() {
   console.log("Application started on port " + app.get("port"));
 });
 
-http.createServer(app).listen(8080, function() {
-  console.log("Application started and listening on port 8080");
+http.createServer(app).listen(app.get("port"), function() { console.log("Application started and listening on port " + app.get("port")) });
 });
